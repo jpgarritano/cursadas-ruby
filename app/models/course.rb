652a65ce02f350524be667042dc89157a554c8b8
Juan Pablo <tuny22@gmail.com>
2018-02-18 05:46:48 -0300

    borro comentarios

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,6 @@
 class Course < ApplicationRecord
 	has_many :tests, dependent: :restrict_with_exception
-	#has_many :enrolments
-	#has_many :students, through: :enrolments
+
 	has_and_belongs_to_many :students, -> { distinct }
 
 	validates :year, uniqueness: true, numericality: true
